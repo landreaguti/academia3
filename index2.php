3e13060e49bddac6904baa221a95c7c04a3acc5d
landreaguti <laura.gutierrez9824@gmail.com>
2021-08-27 12:12:17 -0500

    opcion de grado

---
+++ index2.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+
+	<?php
+	session_start();
+	if (@!$_SESSION['user']) {
+		header("Location:index.php");
+	}elseif ($_SESSION['rol']==1) {
+		header("Location:admin.php");
+	}
+	?>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Proyecto Academias</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Joseph Godoy">
+
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="bootstrap/js/jquery-1.8.3.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+  </head>
+<body data-offset="40" background="images/fondotot.jpg" style="background-attachment: fixed">
+<div class="container">
+<header class="header">
+<div class="row">
+	<?php
+	include("include/cabecera.php");
+	?>
+</div>
+</header>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+  <!-- Navbar
+    ==<br><br><br><br>================================================ -->
+<?php
+
+include("include/menu.php");
+
+?>
+
+
+<!-- ======================================================================================================================== -->
+<?php
+
+include("include/index.html");
+
+?>
+<?php
+
+include("include/chart-chartjs.html");
+
+?>
+
+<!-- Footer
+      ================================================== -->
+<hr class="soften"/>
+<footer class="footer">
+
+<hr class="soften">
+
+ </footer>
+</div><!-- /container -->
+
+	</style>
+  </body>
+</html>
